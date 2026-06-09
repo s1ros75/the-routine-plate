9b282439996d9748de79861891516f1db3e62d63
sunago.soichiro@gmail.com <you@example.com>
2026-06-10 01:54:57 +0900

    fix: Add db:seed to entrypoint for production deployment

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 set -e
-
 rm -f /app/tmp/pids/server.pid
-
-bundle exec rails db:create db:migrate
-
+bundle exec rails db:create db:migrate db:seed
 exec bundle exec rails server -b 0.0.0.0 -p 3000
